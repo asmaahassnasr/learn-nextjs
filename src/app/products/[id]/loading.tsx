8acfad0cac0f63dcf88dbc0b55c1eb6c9f39694f
ASM@GitHub2024 <asmaa14has@gmail.com>
2025-10-01 01:39:21 +0300

    loading UI

---
+++ src/app/products/[id]/loading.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LoadingProductDetailsPage = () => {
+  return (
+    <div>Product ###### ..........</div>
+  )
+}
+
+export default LoadingProductDetailsPage
---
+++ src/app/products/loading.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LoadingProductsPAge = () => {
+  return (
+    <div>All Products ....</div>
+  )
+}
+
+export default LoadingProductsPAge
